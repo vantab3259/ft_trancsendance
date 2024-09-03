d3e3404a65e76d2e0ece327ec4feaadab2eb5574
ranki <rabalone94@gmail.com>
2024-09-03 17:21:03 +0200

    add header in dashboard

---
+++ srcs/django/mysite/static/js/dashboard/dashboard.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var progressBar = document.querySelector('.progress-bar');
+    var progress = progressBar.querySelector('.progress');
+    var value = progressBar.getAttribute('value');
+    progress.style.width = value + '%';
+});
